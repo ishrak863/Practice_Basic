817eceb9f4603b15b46ad950c3e21ff6c5ea156e
Farhan Ishrak <58267004+ishrak863@users.noreply.github.com>
2022-12-04 21:21:09 +0600

    Add files via upload

---
+++ comma_separated_string_word.py
@@ -0,0 +1,8 @@
+import pandas as pd
+a= input()
+a= a.lower()
+result = [x.strip() for x in a.split(',')]
+s = pd.Series(result)
+d= s.value_counts()
+print (d)
+#d.to_excel(r'H:\Python Programs\test_data.xlsx')
